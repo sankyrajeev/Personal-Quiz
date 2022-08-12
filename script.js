ad7ac9af9f42dcc8e2bf683f90bb44075d2de747
Vontell <sankarsh.rajeev@gmail.com>
2022-08-12 17:53:01 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,50 @@
+var questions = [
+    {
+        numb : 1,
+        question: "Question 1?",
+        answer: "answer 1",
+        options:[
+            "answer 1",
+            "answer x",
+            "answer y",
+            "answer r"
+        ]
+    },
+
+    {
+        numb : 2,
+        question: "Question 2?",
+        answer: "answer 1",
+        options:[
+            "answer 1",
+            "answer x",
+            "answer y",
+            "answer r"
+        ]
+    },
+    {
+        numb : 3,
+        question: "Question 3?",
+        answer: "answer 1",
+        options:[
+            "answer 1",
+            "answer x",
+            "answer y",
+            "answer r"
+        ]
+    },
+
+    {
+        numb : 4,
+        question: "Question 4?",
+        answer: "answer 1",
+        options:[
+            "answer 1",
+            "answer x",
+            "answer y",
+            "answer r"
+        ]
+    },
+
+
+]
